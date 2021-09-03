34a0ec941f081928fd997df519662319a30129d1
Zloysvin <danylomartakov@gmil.com>
2021-09-03 18:32:57 +0300

    goodbye world

--- testprj/testprj/Program.cs
+++ testprj/testprj/Program.cs
@@ -6,7 +6,7 @@ namespace testprj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye World!");
         }
     }
 }
